4419cc1998ac9c679fdfd2a1a5d9419051b89459
Jan Poonthong <janpoonthong628@gmail.com>
2021-12-02 12:49:47 +0700

    Add iterative function

--- chapter-1.2/factorial.py
+++ chapter-1.2/factorial.py
@@ -6,3 +6,14 @@ def fact(n):
         return n * fact(n-1)
 
 print(fact(4))
+
+# Iterative
+def fact_i(n):
+    def fact_iter(acc, n):
+        if n == 0:
+            return acc
+        else:
+            return fact_iter(n*acc, n-1)
+    return fact_iter(1, n)
+
+print(fact_i(4))
